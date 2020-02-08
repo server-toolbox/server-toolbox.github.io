dd7588f1fbbb226bc0556cf5c3016900f9cb954b
KaMeHb-UA <marlock@etlgr.com>
2020-02-08 18:33:41 +0200

    path component

---
+++ app/components/path.js
@@ -0,0 +1,12 @@
+/** @arg {string} dir */
+export function up(dir){
+    const pieces = dir.split('/');
+    pieces.pop();
+    return pieces.join('/')
+}
+
+export const components = up(import.meta.url)
+
+export const app = up(components)
+
+export const containers = app + '/containers'
