74a3e3a474fbb39704814ec7cd68a7af9f984e0e
KaMeHb <marlock@etlgr.com>
2020-02-11 11:59:18 +0200

    real script name

--- app/components/yaml.js
+++ app/components/yaml.js
@@ -1,4 +1,4 @@
 import Thread from './threads.js'
 import { components } from './path.js'
 
-export default new Thread(components + '/yaml/js-yaml.js')
+export default new Thread(components + '/yaml/js-yaml.js', components + '/3rd-party/js-yaml.min.js')
