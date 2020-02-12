9a92e1ab29d96d6ace15a57e75815adf3bc63db4
KaMeHb-UA <marlock@etlgr.com>
2020-02-13 00:24:21 +0200

    initial servers

---
+++ app/components/servers.js
@@ -0,0 +1,88 @@
+const _prefix = Symbol();
+
+class Server{
+    constructor(prefix){
+        this[_prefix] = prefix
+    }
+
+    get name(){
+        return localStorage[this[_prefix] + 'name']
+    }
+    set name(v){
+        localStorage[this[_prefix] + 'name'] = v
+    }
+
+    get host(){
+        return localStorage[this[_prefix] + 'host']
+    }
+    set host(v){
+        localStorage[this[_prefix] + 'host'] = v
+    }
+
+    get port(){
+        return +localStorage[this[_prefix] + 'port']
+    }
+    set port(v){
+        localStorage[this[_prefix] + 'port'] = v
+    }
+
+    get path(){
+        return localStorage[this[_prefix] + 'path']
+    }
+    set path(v){
+        localStorage[this[_prefix] + 'path'] = v
+    }
+
+    get authMethod(){
+        return localStorage[this[_prefix] + 'authMethod']
+    }
+    set authMethod(v){
+        localStorage[this[_prefix] + 'authMethod'] = v
+    }
+
+    get user(){
+        return localStorage[this[_prefix] + 'user']
+    }
+    set user(v){
+        localStorage[this[_prefix] + 'user'] = v
+    }
+
+    get password(){
+        return localStorage[this[_prefix] + 'password']
+    }
+    set password(v){
+        localStorage[this[_prefix] + 'password'] = v
+    }
+
+    get key(){
+        return localStorage[this[_prefix] + 'key']
+    }
+    set key(v){
+        localStorage[this[_prefix] + 'key'] = v
+    }
+
+    get passphrase(){
+        return localStorage[this[_prefix] + 'passphrase']
+    }
+    set passphrase(v){
+        localStorage[this[_prefix] + 'passphrase'] = v
+    }
+
+    get ssl(){
+        return Boolean(+localStorage[this[_prefix] + 'ssl'])
+    }
+    set ssl(v){
+        localStorage[this[_prefix] + 'ssl'] = +v
+    }
+
+    formatURL(){
+        const url = new URL('http://l');
+        url.protocol = 'ws' + (this.ssl ? 's' : '');
+        url.host = this.host;
+        url.port = this.port;
+        url.pathname = this.path;
+        return url.href
+    }
+}
+
+export default [...new Array(+localStorage.servers || 0)].map((_, i) => new Server('server' + i))
