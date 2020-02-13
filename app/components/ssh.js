f3829a50b0f6eb7953f7902e7d8fc87267f9e240
KaMeHb <marlock@etlgr.com>
2020-02-13 12:25:58 +0200

    server is specified while constructor called

--- app/components/ssh.js
+++ app/components/ssh.js
@@ -1,20 +1,16 @@
 import Thread from './threads.js'
 import { components } from './path.js'
-import servers from './servers.js'
 
-const server = servers[0];
 const thread = Symbol();
 
 export default class{
-    constructor(){
-        this[thread] = new Thread(components + '/ssh/thread.js', components + '/3rd-party/ssh2.js')
-    }
-    connect(){
-        this[thread].connect({
-            host: server.formatURL(),
+    constructor(server){
+        this[thread] = new Thread(components + '/ssh/thread.js', components + '/3rd-party/ssh2.js');
+        this[thread].setAddr(server.formatURL()).then(() => this[thread].connect({
+            host: server.host,
             username: server.user,
             privateKey: server.key,
             passphrase: server.passphrase,
-        })
+        }))
     }
 }
