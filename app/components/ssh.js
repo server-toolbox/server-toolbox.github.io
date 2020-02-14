3e5ac60878877062277b4dff3b459800489b6bc7
KaMeHb <marlock@etlgr.com>
2020-02-14 14:27:30 +0200

    there is no need to wait for connect because worker do

--- app/components/ssh.js
+++ app/components/ssh.js
@@ -2,20 +2,18 @@ import Thread from './threads.js'
 import { components } from './path.js'
 
 const thread = Symbol();
-const connected = Symbol();
 
 export default class{
     constructor(server){
         this[thread] = new Thread(components + '/ssh/thread.js', components + '/3rd-party/ssh2.js');
-        this[connected] = this[thread].setAddr(server.formatURL()).then(() => this[thread].connect({
+        this[thread].setAddr(server.formatURL()).then(() => this[thread].connect({
             host: server.host,
             username: server.user,
             privateKey: server.key,
             passphrase: server.passphrase,
         }))
     }
-    async exec(command){
-        await this[connected];
+    exec(command){
         return this[thread].exec(command)
     }
 }
