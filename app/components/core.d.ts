8429cd80985038876048b3e4fd95b69b877d13c6
KaMeHb-UA <marlock@etlgr.com>
2020-02-15 20:58:26 +0200

    removed core wasm module

--- app/components/core.d.ts
+++
@@ -1,6 +0,0 @@
-export default class Core{
-    /**
-     * Connect to SSH server through WebSocket
-     */
-    connect(host: string, path?: string, port?: number | string, ssl?: boolean): Promise<void>
-}
