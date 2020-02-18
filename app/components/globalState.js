574ebcdc766719118b1ee479cc5b33af08d76f80
KaMeHb-UA <marlock@etlgr.com>
2020-02-19 00:20:47 +0200

    changing global state refreshes connected elements

--- app/components/globalState.js
+++ app/components/globalState.js
@@ -1,13 +1,50 @@
 import { html, Component } from './3rd-party/preact.js'
 import withLog from './logger/index.js'
 
-const GLOBAL_STATE = new Map();
+const connectedElements = new Map();
+
+function updateConnectedElements(){
+    for(const [ element ] of connectedElements) element.forceUpdate()
+}
+
+function log(state, ...args){
+    console.group('GLOBAL_STATE');
+    console.log(...args);
+    console.log('STATE: %O', new Map(state));
+    console.groupEnd()
+}
+
+class State extends Map{
+    set(key, value){
+        log(this, 'Set key: %O value %O', key, value);
+        updateConnectedElements();
+        super.set(key, value)
+    }
+    delete(key){
+        log(this, 'Deleted key: %O', key);
+        updateConnectedElements();
+        super.delete(key)
+    }
+    clear(){
+        log(this, 'Cleared');
+        updateConnectedElements();
+        super.clear()
+    }
+}
+
+const GLOBAL_STATE = new State();
 
 let modulesRegister = Object.create(null);
 let asyncComponentsLoaded = false;
 
 export function connect(stateDescriptor){
     return _ => class extends Component{
+        componentDidMount(){
+            connectedElements.set(this, undefined)
+        }
+        componentWillUnmount(){
+            connectedElements.delete(this, undefined)
+        }
         render(){
             return html`<${_} ...${this.props} ...${stateDescriptor(GLOBAL_STATE)}/>`
         }
