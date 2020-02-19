b046e16be9cd494f157f6fd9f33a354d9d3cda73
KaMeHb <marlock@etlgr.com>
2020-02-19 13:02:27 +0200

    load realtime update

--- app/containers/servers/index.js
+++ app/containers/servers/index.js
@@ -1,6 +1,8 @@
 import { html, Component } from '../../components/3rd-party/preact.js'
 import servers from '../../components/servers.js'
 import { connect } from '../../components/globalState.js'
+import { setIntervalImmediate } from '../../components/helpers.js'
+import { memUsage, cpuUsage } from '../../components/remoteDataControllers.js'
 
 class Percentage extends Component{
     render(){
@@ -10,11 +12,31 @@ class Percentage extends Component{
 }
 
 const Server = class extends Component{
+    state={
+        load: 0,
+    }
+    componentDidMount(){
+        this.interval = setIntervalImmediate(async () => {
+            const { connection } = this.props;
+            const [ { used, total, usedSwap, totalSwap }, {cpu} ] = await Promise.all([
+                memUsage(connection),
+                cpuUsage(connection),
+            ]);
+            const memUsed = Math.round((used + usedSwap) / (total + totalSwap) * 100);
+            this.setState({
+                load: Math.round(Math.max(memUsed, cpu))
+            })
+        }, 2000)
+    }
+    componentWillUnmount(){
+        clearInterval(this.interval)
+    }
     render(){
-        const { server, connection, translate } = this.props;
+        const { server, translate } = this.props;
+        const { load } = this.state;
         return html`<div class=mdl-block>
             ${server.name}
-            <span class=load>${translate('servers.load')} <${Percentage} value=${81}/></span>
+            <span class=load>${translate('servers.load')} <${Percentage} value=${load}/></span>
         </div>`
     }
 }
