de4f69b2ed49202efaac830ebce4ab6477815aac
KaMeHb <marlock@etlgr.com>
2020-03-04 16:57:32 +0200

    set first active if there no active

--- app/components/material/switch/index.js
+++ app/components/material/switch/index.js
@@ -6,7 +6,7 @@ export default class extends Component{
         const [ key1, key2 ] = Object.keys(values);
         const {[key1]: val1, [key2]: val2} = values;
         return html`<div class='material switch'>
-            <div onclick=${() => onInput && onInput(key1)} class=${active === key1 ? 'active' : ''}>${val1}</div>
+            <div onclick=${() => onInput && onInput(key1)} class=${active === undefined || active === key1 ? 'active' : ''}>${val1}</div>
             <div onclick=${() => onInput && onInput(key2)} class=${active === key2 ? 'active' : ''}>${val2}</div>
         </div>`
     }
