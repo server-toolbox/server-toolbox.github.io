0ce612a678e0c7f2f07100c349365d31de8f9cc0
KaMeHb <marlock@etlgr.com>
2020-03-05 11:26:56 +0200

    docker item deprecated, replaced with containers

--- app/containers/menu/index.js
+++ app/containers/menu/index.js
@@ -1,6 +1,5 @@
 import { html, Component } from '../../components/3rd-party/preact.js'
 import Link from './link.js'
-import DockerIcon from '../../components/icons/docker.js'
 
 export default class Menu extends Component{
     render(){
@@ -14,7 +13,7 @@ export default class Menu extends Component{
             <nav>
                 <${Link} icon=home name=home ...${_}/>
                 <${Link} icon=storage name=servers ...${_}/>
-                <${Link} icon=${html`<${DockerIcon}/>`} name=docker ...${_}/>
+                <${Link} icon=developer_board name=containers ...${_}/>
             </nav>
         `
     }
