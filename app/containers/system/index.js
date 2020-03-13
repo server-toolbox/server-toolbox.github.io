89b7c7550d9e4151d85ffc45014333c67fb61dd8
KaMeHb <marlock@etlgr.com>
2020-03-13 12:47:52 +0200

    upd

--- app/containers/system/index.js
+++ app/containers/system/index.js
@@ -3,11 +3,37 @@ import { connect } from '../../components/globalState.js'
 import { sysInfo } from '../../components/remoteDataControllers.js'
 
 class Home extends Component{
+    state = {
+        info: [],
+    }
+    async componentDidMount(){
+        this.props.connections.forEach((c, i) => {
+            sysInfo(c).then(v => {
+                const newInfo = Array.from(this.state.info);
+                newInfo[i] = v;
+                this.setState({ info: newInfo })
+            })
+        })
+    }
     render(){
-        const { connections, servers, translate } = this.props;
-        const sysInfos = connections.map(c => sysInfo.bind(null, c));
-        console.log({ sysInfos });
-        return html`<div class='material block'>sys info</div>`
+        const { servers, translate } = this.props;
+        return servers.map((server, i) => {
+            const info = this.state.info[i];
+            if(!info) return null;
+            return html`<div class='material block system-info'>
+                <div class=basename>${server.name}</div>
+                <div>
+                    <div class=osicon style=${`background-image:url(${info.os.icon})`}/>
+                    <div class=osname>${info.os.name} ${info.os.codename}</div>
+                    <div class=osver>${translate('system.os.version')} ${info.os.version}</div>
+                    <div class=ostype>${translate('system.os.type')} ${info.os.type}</div>
+                    <a href=${info.os.links.home} target=blank>${translate('system.os.homepage')}</a>
+                    <a href=${info.os.links.support} target=blank>${translate('system.os.supportpage')}</a>
+                </div>
+                <div>
+                </div>
+            </div>`
+        })
     }
 }
 
